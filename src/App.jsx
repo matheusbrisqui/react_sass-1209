3a47a3019482d8948a85a0defa68337a605df338
Logon Aluno <mabrisqui@gmail.com>
2023-09-12 11:40:26 -0300

    Projeto react e sass dia 12/09

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <section className="estrutura">
+          <article className="box">
+            <h1>Nossa estrutura</h1>
+            <img src="https://fastly.picsum.photos/id/1/350/200.jpg?hmac=jXaBDOH_nxvv3bnF2rR193RGxOxrRNYuqyV5gbEGK4o" alt="Error"/>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </p>
+            <a href="#">Veja mais</a>
+          </article>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
